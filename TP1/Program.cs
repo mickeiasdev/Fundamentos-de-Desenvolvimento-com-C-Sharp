2b60d459f6f8faccd6c3a6d3930e3e1423f2e177
Mickeias Dev <mickeiasdev@gmail.com>
2025-11-03 21:36:12 -0300

    commit

--- TP1/Program.cs
+++ TP1/Program.cs
@@ -20,45 +20,45 @@ public class Program
 
             Console.Clear();
 
-                switch (escolha)
-                {
-                    case "1":
-                        E1.Run();
-                        break;
-                    case "2":
-                        E2.Run();
-                        break;
-                    case "3":
-                        E3.Run();
-                        break;
-                    case "4":
-                        E4.Run();
-                        break;
-                    case "5":
-                        E5.Run();
-                        break;
-                    case "6":
-                        E6.Run();
-                        break;
-                    case "7":
-                        E7.Run();
-                        break;
-                    case "8":
-                        E8.Run();
-                        break;
-                    case "9":
-                        E9.Run();
-                        break;
-                    case "10":
-                        E10.Run();
-                        break;
-                    case "0":
-                        Console.WriteLine("Finalizando.");
-                        return;
-                    default:
-                        Console.WriteLine("Opção inválida! Tente um número de 1 a 10.");
-                        break;
-                }
+            switch (escolha)
+            {
+                case "1":
+                    E1.Run();
+                    break;
+                case "2":
+                    E2.Run();
+                    break;
+                case "3":
+                    E3.Run();
+                    break;
+                case "4":
+                    E4.Run();
+                    break;
+                case "5":
+                    E5.Run();
+                    break;
+                case "6":
+                    E6.Run();
+                    break;
+                case "7":
+                    E7.Run();
+                    break;
+                case "8":
+                    E8.Run();
+                    break;
+                case "9":
+                    E9.Run();
+                    break;
+                case "10":
+                    E10.Run();
+                    break;
+                case "0":
+                    Console.WriteLine("Finalizando.");
+                    return;
+                default:
+                    Console.WriteLine("Opção inválida! Tente um número de 1 a 10.");
+                    break;
+            }
             
 
             Console.WriteLine("\n----------------------------------------");
